aa76074894f53fc50b968dc42f3ac01581626de5
pohii <kp51875@fer.hr>
2025-05-14 17:20:22 +0200

    parsing

--- hirgc.cpp
+++ hirgc.cpp
@@ -1,9 +1,26 @@
 // hirgc_single.cpp
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char *argv[])
-{
-    std::cerr << "Usage:\n";
+int main(int argc, char* argv[]) {
+    if (argc < 5) {
+        std::cerr << "Usage:\n"
+                  << "  " << argv[0] << " compress <ref.fa> <target.fa> <out.bin>\n"
+                  << "  " << argv[0] << " decompress <ref.fa> <in.bin> <out.fa>\n";
+        return 1;
+    }
+
+    std::string mode = argv[1];
+    if (mode == "compress") {
+        // compress logic to be implemented
+    } else if (mode == "decompress") {
+        // decompress logic to be implemented
+    } else {
+        std::cerr << "Unknown mode: " << mode << std::endl;
+        return 1;
+    }
+
     return 0;
 }
